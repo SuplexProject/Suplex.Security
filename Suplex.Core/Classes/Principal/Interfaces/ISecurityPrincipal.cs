42e5d6c00c3a58f516225f477b01dba6f8c9ec5d
Steve <steve.shortt@gmail.com>
2018-03-24 03:28:10 -0500

    removed SecurityPrincipal.Id

--- Suplex.Core/Classes/Principal/Interfaces/ISecurityPrincipal.cs
+++ Suplex.Core/Classes/Principal/Interfaces/ISecurityPrincipal.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Suplex.Security.Principal
 {
     public interface ISecurityPrincipal
     {
         Guid? UId { get; set; }
-        int Id { get; set; }
+        //int Id { get; set; }
         string Name { get; set; }
         string Description { get; set; }
         bool IsLocal { get; set; }
