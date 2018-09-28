1aab5df72d13a5b3a6089917812a88b273d0687c
Steve <steve.shortt@gmail.com>
2018-09-28 11:58:53 -0500

    Convience prop on IRightInfo

--- Suplex.Security.Core/Classes/AclModel/Interfaces/IRightInfo.cs
+++ Suplex.Security.Core/Classes/AclModel/Interfaces/IRightInfo.cs
@@ -5,6 +5,7 @@ namespace Suplex.Security.AclModel
     public interface IRightInfo
     {
         string FriendlyTypeName { get; }
+        string FriendlyTypeNameValue { get; }
         Type RightType { get; }
         int Value { get; }
         string Name { get; }
--- Suplex.Security.Core/Classes/AclModel/RightInfo.cs
+++ Suplex.Security.Core/Classes/AclModel/RightInfo.cs
@@ -5,9 +5,15 @@ namespace Suplex.Security.AclModel
     public class RightInfo<T> : IRightInfo where T : struct, IConvertible
     {
         public T Right { get; set; }
+        public string Name { get { return Right.ToString(); } }
         public string FriendlyTypeName { get { return Right.GetFriendlyRightTypeName(); } }
         public Type RightType { get { return Right.GetType(); } }
         public int Value { get { return (int)Enum.Parse( Right.GetType(), Right.ToString() ); } }
-        public string Name { get { return Right.ToString(); } }
+        public string FriendlyTypeNameValue { get { return $"{FriendlyTypeName}\\{Name}"; } }
+
+        public override string ToString()
+        {
+            return FriendlyTypeNameValue;
+        }
     }
 }
